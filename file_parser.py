dceec052a74d414bafb679376aa5870582fed129
klymentenkokate <100036954+klymentenkokate@users.noreply.github.com>
2022-05-03 20:39:40 +0300

    Add files via upload

---
+++ file_parser.py
@@ -0,0 +1,115 @@
+import sys
+from pathlib import Path
+
+JPEG_IMAGES = []
+JPG_IMAGES = []
+PNG_IMAGES = []
+SVG_IMAGES = []
+AVI_VIDEO = []
+MP4_VIDEO = []
+MOV_VIDEO = []
+MKV_VIDEO = []
+DOC_DOCS = []
+DOCX_DOCS = []
+TXT_DOCS = []
+PDF_DOCS = []
+XLSX_DOCS = []
+PPTX_DOCS = []
+MP3_AUDIO = []
+OGG_AUDIO = []
+WAV_AUDIO = []
+AMR_AUDIO = []
+OTHER = []
+ARCHIVES = []
+
+REGISTER_EXTENSIONS = {
+    'JPEG': JPEG_IMAGES,
+    'PNG': PNG_IMAGES,
+    'JPG': JPG_IMAGES,
+    'SVG': SVG_IMAGES,
+    'AVI': AVI_VIDEO,
+    'MP4': MP4_VIDEO,
+    'MOV': MOV_VIDEO,
+    'MKV': MKV_VIDEO,
+    'DOC': DOC_DOCS,
+    'DOCX': DOCX_DOCS,
+    'TXT': TXT_DOCS,
+    'PDF': PDF_DOCS,
+    'XLSX': XLSX_DOCS,
+    'PPTX': PPTX_DOCS,
+    'OGG': OGG_AUDIO,
+    'WAV': WAV_AUDIO,
+    'AMR': AMR_AUDIO,
+    'MP3': MP3_AUDIO,
+    'ZIP': ARCHIVES,
+    'GZ': ARCHIVES,
+    'TAR': ARCHIVES
+}
+
+FOLDERS = []
+EXTENSIONS = set()
+UNKNOWN = set()
+
+
+def get_extension(filename: str) -> str:
+    # превращаем расширение файла в название папки .jpg -> JPG
+    return Path(filename).suffix[1:].upper()
+
+
+def scan(folder: Path) -> None:
+    for item in folder.iterdir():
+        # Если это папка то добавляем ее с список FOLDERS и преходим к следующему элементу папки
+        if item.is_dir():
+            # проверяем, чтобы папка не была той в которую мы складываем уже файлы
+            if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'OTHER'):
+                FOLDERS.append(item)
+                #  сканируем эту вложенную папку - рекурсия
+                scan(item)
+            #  перейти к следующему элементу в сканируемой папке
+            continue
+
+        #  Пошла работа с файлом
+        ext = get_extension(item.name)  # взять расширение
+        fullname = folder / item.name  # взять полный путь к файлу
+        if not ext:  # если у файла нет расширения добавить к неизвестным
+            OTHER.append(fullname)
+        else:
+            try:
+                # взять список куда положить полный путь к файлу
+                container = REGISTER_EXTENSIONS[ext]
+                EXTENSIONS.add(ext)
+                container.append(fullname)
+            except KeyError:
+                # Если мы не регистрировали расширение в REGISTER_EXTENSIONS, то добавить в другое
+                UNKNOWN.add(ext)
+                OTHER.append(fullname)
+
+
+if __name__ == '__main__':
+    folder_for_scan = sys.argv[1]
+    print(f'Start in folder {folder_for_scan}')
+
+    scan(Path(folder_for_scan))
+    print(f'Images jpeg: {JPEG_IMAGES}')
+    print(f'Images jpg: {JPG_IMAGES}')
+    print(f'Images svg: {SVG_IMAGES}')
+    print(f'Document doc: {DOC_DOCS}')
+    print(f'Document docx: {DOCX_DOCS}')
+    print(f'Document txt: {TXT_DOCS}')
+    print(f'Document pdf: {PDF_DOCS}')
+    print(f'Document xlsx: {XLSX_DOCS}')
+    print(f'Document pptx: {PPTX_DOCS}')
+    print(f'Video avi: {AVI_VIDEO}')
+    print(f'Video mp4: {MP4_VIDEO}')
+    print(f'Video mov: {MOV_VIDEO}')
+    print(f'Video mkv: {MKV_VIDEO}')
+    print(f'Audio mp3: {MP3_AUDIO}')
+    print(f'Audio ogg: {OGG_AUDIO}')
+    print(f'Audio wav: {WAV_AUDIO}')
+    print(f'Audio mp3: {AMR_AUDIO}')
+    print(f'Archives: {ARCHIVES}')
+
+    print(f'Types of files in folder: {EXTENSIONS}')
+    print(f'Unknown files of types: {UNKNOWN}')
+
+    print(FOLDERS[::-1])
